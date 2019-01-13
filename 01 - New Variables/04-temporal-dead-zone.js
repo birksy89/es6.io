377cc46309089fccc513b97aea950d4dfda0d33a
Andrew <andy.birks@gmail.com>
2019-01-13 14:44:11 +0000

    Module 1 - Complete

---
+++ 01 - New Variables/04-temporal-dead-zone.js
@@ -0,0 +1,10 @@
+//The temporal dead zone is the area where you cannot access a variable before it is defined
+console.log(pizza2);
+var pizza2 = 'Deep Dish Pizza 🍕🍕🍕'
+
+
+
+var pizza = 'Deep Dish Pizza 🍕🍕🍕'
+console.log(pizza);
+
+
